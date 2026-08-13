55c95e8473358eb9f895eb2fb2de8ddc33788939
Robert Santos <robertsantosti@outlook.com>
2026-08-13 17:29:41 -0300

    chore(vite): substituir __dirname por import.meta.dirname

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   base: "/",
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": resolve(import.meta.dirname!, "src"),
     },
   },
   build: {
